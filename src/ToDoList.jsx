f33572936df5608d3516f4bdc9628e1b3759bab4
Guilherme Alves <guilherme_alves_08@hotmsil.com>
2025-03-24 11:01:02 -0300

    Adicionando comandos de adição, exclusão e contagem

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,25 +1,44 @@
-
-import Teacher1 from "./assets/Vector.svg"
-import Teacher2 from "./assets/delete.svg"
+import React, {useState} from "react"
+import edit from "./assets/Vector.svg"
+import del from "./assets/delete.svg"
 
 
 function ToDoList(){
+
+        const [tasks, setTasks] = useState([]);
+        const [newTask, setNewTask] = useState("");
+
+        function handleInputChange(event){
+            setNewTask(event.target.value);
+        }
+
+        function addTask(){
+
+            if(newTask.trim() !== ""){
+                setTasks(t => [...t, newTask]);
+                setNewTask("");
+            }
+            
+        }
+
+        function deleteTask(index){
+            const updatedTasks = tasks.filter((_, i) => i !== index);
+            setTasks(updatedTasks);
+        } 
  
-
-
-
         return(
 
-            
+            //Menu superior
             <div className="to-do-list">
                 <div className="header">
                         <span>Organização</span>
                         <span>Tarefas</span>
                 </div>
 
-                <h1>Otimize seu tempo e se organize com o nosso Planejador Diário .</h1>
+            <h1>Otimize seu tempo e se organize com o nosso Planejador Diário .</h1>
 
-            <div id="form">
+            
+            <div id="form"> 
                 <ul className="title">
                            <li className="title2">Tarefas</li>
                            <li className="title2">Status</li>
@@ -30,32 +49,30 @@ function ToDoList(){
 
                 <ol>
           
-                    
-                        <li>
+          {tasks.map((task, index) => 
+              <li key={index}>
+                  
+           
+                  <span className="text">{task}</span>
+                  <div><input type="checkbox"></input></div>
+                  
+                  <div><button className="delete-button"><img src={edit } width="20.42" height="22.5" /></button>
+                  <button className="delete-button"><img src={del }  width="20.42" height="22.5" onClick={deleteTask()}/></button></div>
+                  
+                  
+              </li>
                             
-                     
-                            <span className="text"></span>
-                            <div><input type="checkbox"></input></div>
-                            
-                            <div><button className="delete-button"><img src={Teacher1 } width="20.42" height="22.5" /></button>
-                            <button className="delete-button"><img src={Teacher2 }  width="20.42" height="22.5"/></button></div>
-                            
-                            
-                        </li>
-                                      
-        
-                         
-                </ol>
-        
-                
+          )}
+               
+      </ol>
+                  
                 <div id="add">
-                    <input type="text" placeholder="Nova tarefa"/>
-                    <button className="add-button">+</button>
+                    <input type="text" placeholder="Nova tarefa" value={newTask} onChange={handleInputChange}/>
+                    <button className="add-button" onClick={addTask}>+</button>
   
                 </div>
 
                 </div>             
-        
         </div>)
 }
 
